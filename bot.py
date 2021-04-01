6575b6f940dd0c86847d6c1df92042aca411c9e4
davidstanke <davidstanke@gmail.com>
2021-04-01 19:58:40 +0000

    force overwrite with game code

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os, random, string, logging, collections, asyncio, jsonpickle
+import os, random, string, logging, collections, asyncio, # jsonpickle
 from collections import defaultdict
 from os import path
 
@@ -8,143 +8,119 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-description = """A bot to help strangers make words out of letters"""
+description = '''A bot to help strangers make words out of letters'''
 
 # set the prefix bot will watch for
-
-bot = commands.Bot(command_prefix="..", description=description)
+bot = commands.Bot(command_prefix='..', description=description)
 
 # make this an environment variable
-token = os.environ["TOKEN"]
+token = ''
 
-pl_letters = defaultdict(list)  # dictionary of lists for letters owned by each player
-all_letters = []  # list to store all letters deployed, for testing
+pl_letters = defaultdict(list) # dictionary of lists for letters owned by each player
+all_letters = [] # list to store all letters deployed, for testing
+valid_words = ['CAT', 'RAT', 'BAT', 'SAT', 'MAT', 'TALL', 'BALL', 'CALL', 'FALL', 'FAR', 'TAR',
+               'BAR', 'CAR', 'CAB', 'TAB', 'LAB', 'GNAT', 'TAN', 'CAN', 'BAN', 'RAN', 'BASS',
+               'MAN', 'APP', 'TART', 'FART', 'THAT', 'SEEN', 'LANE', 'TEEN', 'TALE', 'TEAL', 'FELL'
+               'TELL', 'SET', 'NET', 'EAT', 'BEAT', 'NEAT', 'SEAT', 'TEAR', 'STAR', 'LANE', 'ARE',
+               'SELL', 'SALE', 'SEAL', 'LEER', 'STELLAR', 'TREE', 'SEER', 'PEER', 'PEAR', 'APE',
+               'TINE', 'SINE', 'SIN', 'NIT', 'RISE', 'LINT', 'TILL', 'SILL', 'TIN', 'TIRE', 'AND',
+               'END', 'SAND', 'SEND', 'TEND', 'STAND', 'LET', 'TEN']
+words_i_know = frozenset(valid_words) # used to speed up querying
+letter_weight = { # each integer = percent chance * 10 to appear, 100 = 10%
 
-
-class Game:
-    # game state is stored in a JSON file in folder `.lws` (which is .gitignore'd)
-    # TODO: create alternative cloud-backed storage mechanism
-    statefile = ".lws/gamestate.json"
-
-    def save(self):
-        pickled = jsonpickle.encode(self.state)
-        with open(self.statefile,'w') as statefile:
-            statefile.write(pickled)
-            statefile.close()
-        
-    def load(self):
-        if path.exists(self.statefile):
-            logging.debug("FILE EXISTS")
-            with open(self.statefile,'r') as statefile:
-                self.state = jsonpickle.decode(statefile.read())
-            logging.debug(self.state)
-        else:
-            self.state = {}
-            self.state["pl_letters"] = defaultdict(list) # dictionary of lists for letters owned by each player
-            self.state["all_letters"] = [] # list to store all letters deployed, for testing
-
-    def get_player_letters(self, player):
-        return self.state["pl_letters"][format_player_id(player)]
-
-    def add_player_letter(self, player, letter):
-        self.state["pl_letters"][format_player_id(player)].append(letter)
-        self.state["all_letters"].append(letter)
-
-    def get_all_letters(self):
-        if len(self.state["all_letters"]) == 0:
-            return("You have no letters!")
-        elif len(self.state["all_letters"]) == 1:
-            return("You have 1 letter, which is " + self.state["all_letters"][0] + ".")
-        else:
-            return("You have the letters " + str(" and ".join([", ".join(self.state["all_letters"][:-1]),self.state["all_letters"][-1]] if len(self.state["all_letters"]) > 2 else self.state["all_letters"])) + ".")
-
+    "A": 85, "B": 20, "C": 45, "D": 34, "E": 112, "F": 18, "G": 25, "H": 30, "I": 75, "J": 2,
+    "K": 11, "L": 55, "M": 30, "N": 67, "O": 72, "P": 32, "Q": 2, "R": 76, "S": 57, "T": 69, 
+    "U": 36, "V": 10, "W": 13, "X": 2, "Y": 18, "Z": 2
+}
 
 logging.basicConfig(level=logging.DEBUG)
 
-def format_player_id(player):
-    return f"{player.name}#{player.discriminator}"
-
 @bot.event
 async def on_ready():
-    print("We have logged in as ")
+    print('We have logged in as ')
     print(bot.user.display_name)
-    print("\nLet" "s make some words")
+    print('\nLet''s make some words')
 
-
-"""
-@bot.event
-async def on_message(message):
-    response = str('')
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('hello'):
-        response = 'Hi there, want some letters?'
-
-    if message.content.startswith('letter'):
-        new_letter = await give_letter(message.author)
-        response = 'Your letter is: ' + new_letter
-
-    if message.content.startswith('my letter'):
-        logging.debug("Calling current_letters")
-        response = await current_letters(message.author)
-
-    if message.content.startswith('all letter'):
-        response = await show_all_letter():
-
-    if message.content.startswith('use letter'):
-        response = 'I''m still learning how to make words'
-
-    if message.content.startswith('word'):
-        response = 'you want to make a word, but I don''t know how yet'
-
-    if response:
-        await message.channel.send(response)
-"""
-
-
-@bot.command(description="For getting new letters")
+@bot.command(description='For getting new letters')
 # give player a random letter, adding to their collection
 async def get(ctx):
-    game = Game()
-    game.load()
     player = ctx.author
-    letter_rand = random.choice(string.ascii_uppercase) # TODO: break into function, fix probability
-    game.add_player_letter(player, letter_rand)
-    logging.debug("gave {} to {}".format(letter_rand, player))
-    await ctx.send("Hi {}, you can have a {}".format(player, letter_rand))
-    game.save()
-    return letter_rand
+    letter_rand = await random_letter(player)
+    try:
+        pl_letters[player].append(letter_rand)
+        logging.debug("gave {} to {}".format(letter_rand, player))
+        await ctx.send("{}, you can have a {}".format(player, letter_rand))
+    except:
+        pl_letters[player] = ''
+        logging.debug("# Error 3 #: no letters found for {}".format(player))
+    all_letters.append(letter_rand)
+    return (letter_rand)
 
-
-@bot.command(description="Find out current letters owned by player")
+@bot.command(description='Find out current letters owned by player', aliases=['curr', 'cu'])
 async def current(ctx):
-    game = Game()
-    game.load()
     player = ctx.author
     logging.debug("Fetching letters for {}".format(player))
-    letter_list = game.get_player_letters(player)
-    logging.debug("got letters for {}: {}".format(player, str(letter_list)))
-    await ctx.send("{} your letters are {}".format(player, str(letter_list)))
+    try:
+        letter_list = pl_letters[player]
+        logging.debug("got letters for {}: {}".format(player, str(letter_list)))
+    except:
+        letter_list = []
+    await ctx.send("{} your letters are {}".format(player, str(pl_letters[player])))
 
+@bot.command(description='Make a word')
+async def word(ctx, *args):
+    player = ctx.author
+    word = args[0]
+    if word in words_i_know: # is this a word I think is valid
+        points = len(word)
+        await ctx.send("{} formed the word ""{}"" and scored {} points".format(player, word, points))
+        for l in word: # try to remvoe letters in word from player's inventory
+            ## TODO: need to check only unique letters, avoid duplicates
+            try:
+                pl_letters[player].remove(l.upper())
+            except:
+                await ctx.send("# Error 1 #: Couldn't remove ''{}'' from {}'s letters".format(l, player))
+    else:
+        await ctx.send("# Error 2 #: I don't know the word ""{}"" yet, sorry".format(word))
 
-@bot.command(description="Find out what letters this bot has given out")
+@bot.command(description='Find out what letters this bot has given out')
 async def show_all(ctx):
-    game = Game()
-    game.load()
     logging.debug("Full letter output requested")
-    await ctx.send(game.get_all_letters())
+    await ctx.send(str(all_letters))
 
 @bot.command(description='Hello and introductions')
 async def hello(ctx):
     player = ctx.author
-    await ctx.send(
-        "Hello {}, and welcome to Letters With Strangers. I'm here to help you play the game".format(
-            player
-        )
-    )
+    await ctx.send("Hello {}, and welcome to Letters With Strangers. I'm here to help you play the game".format(player))
+
+#async def check_letters(letters, player):
+
+# Give a semi-random letter, to help people make words
+# TODO: Match proper frequencies for english words, see weight matrix above
 
 
-# async def check_letters(letters, player):
+async def random_letter(author):
+    curr = pl_letters[author]
+    l = ''
+    r = random.randint(1,12)
+    if r == 1 or r == 2:
+        l = 'E'
+    elif r == 3:
+        l = 'A'
+    elif r == 4:
+        l = 'R'
+    elif r == 5:
+        l = 'T'
+    elif r == 6:
+        l = 'N'
+    elif r == 7:
+        l = 'S'
+    elif r == 8:
+        l = 'L'
+    elif r == 9:
+        l = 'I'
+    else:
+        l = random.choice(string.ascii_uppercase)
+    return l
 
-bot.run(token)
+bot.run(token) 
