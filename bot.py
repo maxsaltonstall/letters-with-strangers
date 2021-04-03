344752c01da4a5aa58572cf79f05691c1e9b9bfc
davidstanke <davidstanke@gmail.com>
2021-04-03 20:48:14 +0000

    make a `letter` class Fixes #56

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import os, random, string, logging
+import os, logging
 from player import Player
+from letter import Letter
 
 from discord.ext import commands
 
@@ -25,12 +26,6 @@ valid_words = ['CAT', 'RAT', 'BAT', 'SAT', 'MAT', 'TALL', 'BALL', 'CALL', 'FALL'
                'FIT', 'SIT', 'TIT', 'TAT', 'PAT', 'STALL', 'TEST', 'SEE', 'SEA', 'TEE', 'TEA', 'LEE',
                'TEAT', 'SEAR', 'STILl', 'STALL']
 words_i_know = frozenset(valid_words)  # used to speed up querying to see if word exists
-letter_weight = {  # each integer = percent chance * 10 to appear, 100 = 10%
-
-    "A": 85, "B": 20, "C": 45, "D": 34, "E": 112, "F": 18, "G": 25, "H": 30, "I": 75, "J": 2,
-    "K": 11, "L": 55, "M": 30, "N": 67, "O": 72, "P": 32, "Q": 2, "R": 76, "S": 57, "T": 69,
-    "U": 36, "V": 10, "W": 13, "X": 2, "Y": 18, "Z": 2
-}
 
 # ensure state storage directory exists
 if not os.path.exists(".lws"):
@@ -51,7 +46,7 @@ async def on_ready():
 # Currently up to 8 letters per player
 async def get(ctx):
     player = Player(ctx.author)
-    letter_rand = await random_letter()
+    letter_rand = Letter.random_letter()
     await ctx.send(player.add_letter(letter_rand))
     all_letters.append(letter_rand)
 
@@ -113,31 +108,5 @@ async def hello(ctx):
 async def cheat(ctx):
     player = Player(ctx.author)
     await ctx.send(player.cheat())
-
-
-# Give a semi-random letter, to help people make words
-# TODO: Match proper frequencies for english words, see weight matrix above
-async def random_letter():
-    ltr = ''
-    r = random.randint(1, 13)
-    if r == 1 or r == 2:
-        ltr = 'E'
-    elif r == 3:
-        ltr = 'A'
-    elif r == 4:
-        ltr = 'R'
-    elif r == 5:
-        ltr = 'T'
-    elif r == 6:
-        ltr = 'N'
-    elif r == 7:
-        ltr = 'S'
-    elif r == 8:
-        ltr = 'L'
-    elif r == 9:
-        ltr = 'I'
-    else:
-        ltr = random.choice(string.ascii_uppercase)
-    return ltr
 
 bot.run(token)
---
+++ letter.py
@@ -0,0 +1,34 @@
+import random, string
+
+class Letter:
+    letter_weight = {  # each integer = percent chance * 10 to appear, 100 = 10%
+        "A": 85, "B": 20, "C": 45, "D": 34, "E": 112, "F": 18, "G": 25, "H": 30, "I": 75, "J": 2,
+        "K": 11, "L": 55, "M": 30, "N": 67, "O": 72, "P": 32, "Q": 2, "R": 76, "S": 57, "T": 69,
+        "U": 36, "V": 10, "W": 13, "X": 2, "Y": 18, "Z": 2
+    }
+    
+    @staticmethod
+    # Give a semi-random letter, to help people make words
+    # TODO: Match proper frequencies for english words, see weight matrix above
+    def random_letter():
+        ltr = ''
+        r = random.randint(1, 13)
+        if r == 1 or r == 2:
+            ltr = 'E'
+        elif r == 3:
+            ltr = 'A'
+        elif r == 4:
+            ltr = 'R'
+        elif r == 5:
+            ltr = 'T'
+        elif r == 6:
+            ltr = 'N'
+        elif r == 7:
+            ltr = 'S'
+        elif r == 8:
+            ltr = 'L'
+        elif r == 9:
+            ltr = 'I'
+        else:
+            ltr = random.choice(string.ascii_uppercase)
+        return ltr
