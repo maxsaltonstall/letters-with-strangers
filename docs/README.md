4fccb4c447f9269ecdb8d625261facf1b7bbd385
davidstanke <davidstanke@gmail.com>
2021-05-21 21:28:58 +0000

    add secret manager integration

--- docs/README.md
+++ docs/README.md
@@ -41,23 +41,29 @@ LWS supports two backends for storing player/game state: **local**, for developi
 Game state is stored in JSON files saved in the `.lws` folder (which is excluded from VCS). To use local storage, add the following line to your `.env` file:
 ```
 DATA_STORAGE="local"
+LOCAL_STORAGE_PATH=".lws"
 ```
-(or just omit the DATA_STORAGE variable, since local is the default)
+(or just skip this step, since these are the defaults)
 
 ### Firebase Realtime Database
 Game state is stored in JSON format in [Firebase Realtime Database](https://firebase.google.com/docs/database). 
 
 To set up:
-<!-- 1. Visit [console.cloud.google.com](http://console.cloud.google.com) and create or open a cloud project. -->
 1. Visit [console.firebase.google.com](https://console.firebase.google.com/) and add or open your cloud project.
 1. Open Realtime Database, then click "Create Database" to create a database in "locked mode."
 1. Copy your database URL and add it to your `.env` file:
     ```
-    DATA_STORAGE=https://<your_project_id>-default-rtdb.firebaseio.com/
+    DATA_STORAGE="firebase"
+    FIREBASE_DB_PATH="https://<your_project_id>-default-rtdb.firebaseio.com/"
+    FIREBASE_CREDS_STORAGE="local"
+    FIREBASE_CREDS_PATH="creds/firebase.json"
     ```
 1. Open Settings > Project Settings > [Service Accounts](https://console.firebase.google.com/project/_/settings/serviceaccounts/adminsdk).
 1. Click Generate New Private Key, then confirm by clicking Generate Key.
 1. Save it to `creds/firebase.json`
+
+(TODO: document storing Firebase creds in Secret Manager)
+
 
 ## Linting
 This repo has an Actions config that will apply the flake8 linter to all PRs.
--- models/util/datastore.py
+++ models/util/datastore.py
@@ -1,8 +1,9 @@
-import os, jsonpickle, logging
+import os, jsonpickle, logging, json
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from google.cloud import secretmanager
 
 
 def data_storage() -> str:
@@ -12,23 +13,37 @@ def data_storage() -> str:
 
 # for local data storage, return location of files
 def player_statefile(player_id: int) -> str:
-    return f".lws/player_{player_id}.json"
+    statefile_path=os.environ.get("LOCAL_STORAGE_PATH", ".lws")
+    return f"{statefile_path}/player_{player_id}.json"
 
 
 def party_statefile(party_id: int) -> str:
-    return f".lws/party_{party_id}.json"
+    statefile_path=os.environ.get("LOCAL_STORAGE_PATH", ".lws")
+    return f"{statefile_path}/party_{party_id}.json"
 
 
 # for Firebase Realtime DB, initialize DB
 def init_db():
     if not firebase_admin._apps:  # if not already initialized
-        # Fetch the service account key JSON file contents
-        cred = credentials.Certificate('creds/firebase.json')
+        if os.environ.get("FIREBASE_CREDS_STORAGE","local") == "local":
+            # Fetch the service account key JSON file contents
+            cred = credentials.Certificate(os.environ.get("FIREBASE_CREDS_PATH","creds/firebase.json"))
+        elif os.environ.get("FIREBASE_CREDS_STORAGE") == "secret_manager":
+            
+            # Fetch service account key from Secret Manager
+            sec_client = secretmanager.SecretManagerServiceClient()
+            response = sec_client.access_secret_version(request={"name": os.environ.get("FIREBASE_CREDS_SECRET_MANAGER_VERSION")})
+            service_account_info = json.loads(response.payload.data.decode('utf-8'))
+
+            # build credentials with the service account dict
+            cred = firebase_admin.credentials.Certificate(service_account_info)
+        else:
+            raise Exception("ERROR: Config specifies Firebase data storage, but no firebase credentials were provided.")
+
         # Initialize the app with a service account, granting admin privileges
         firebase_admin.initialize_app(cred, {
-            'databaseURL': data_storage()
+            'databaseURL': os.environ.get("FIREBASE_DB_PATH")
         })
-
 
 def save_player(player_id: int, player_state: dict):
 
@@ -40,10 +55,13 @@ def save_player(player_id: int, player_state: dict):
             statefile.write(pickled)
             statefile.close()
 
-    else:  # store state in Firebase Realtime DB
+    elif data_storage() == "firebase":  # store state in Firebase Realtime DB
         init_db()
         ref = db.reference(f'/player/{player_id}')
         ref.set(pickled)
+    
+    else:
+        raise Exception("ERROR: no data storage configuration specified.")
 
 
 def load_player(player_id: int) -> dict:
@@ -56,8 +74,7 @@ def load_player(player_id: int) -> dict:
         else:
             return None
 
-    else:
-        # load from firebase
+    elif data_storage() == "firebase":  # load state from Firebase Realtime DB
         init_db()
         ref = db.reference(f'/player/{player_id}')
         player = ref.get()
@@ -68,6 +85,9 @@ def load_player(player_id: int) -> dict:
             return jsonpickle.decode(player)
         else:
             return None
+
+    else:
+        raise Exception("ERROR: no data storage configuration specified.")
 
 
 def save_party(party_id: int, party_state: dict):
