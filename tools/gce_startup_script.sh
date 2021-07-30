91c154d0ce294f60131ed6ca7799b313e4cfef7e
Dave Stanke <davidstanke@gmail.com>
2021-07-30 22:40:17 +0000

    add startup script and deploy script

--- tools/gce_startup_script.sh
+++ tools/gce_startup_script.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-mkdir -p /opt/app
 
-# TODO: only install stackdriver if it isn't already installed
-# Install Stackdriver logging agent
-curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-bash install-logging-agent.sh
-
-# TODO: if the source isn't already on disk, clone the most recent tag
-# Fetch source code
-# export HOME=/root
-# git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
+# Install Stackdriver logging agent if not already installed
+if ( ! systemctl -q is-active google-fluentd.service); then
+    curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+    bash install-logging-agent.sh
+fi
 
 # Install or update needed software
 apt-get update
 apt-get install -yq git supervisor python3 python3-pip
 pip3 install --upgrade virtualenv
+
+# if application is not already on disk, clone the latest tag
+if [ ! -d "/opt/app/" ]; then
+    latest_tag=$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/maxsaltonstall/letters-with-strangers/releases/latest))
+    git clone -b ${latest_tag} https://github.com/maxsaltonstall/letters-with-strangers.git /opt/app
+fi
 
 service supervisor start
 
@@ -30,9 +31,11 @@ source /opt/app/env/bin/activate
 /opt/app/env/bin/pip install -r /opt/app/requirements.txt
 
 # populate application config
-touch /opt/app/.env
+DISCORD_TOKEN_SECRET_NAME=$(curl 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/DISCORD_TOKEN_SECRET' --silent --fail --show-error --header 'Metadata-Flavor: Google')
+DATASTORE_NAMESPACE=$(curl 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/DATASTORE_NAMESPACE' --silent --fail --show-error --header 'Metadata-Flavor: Google')
+echo "TOKEN=$(gcloud secrets versions access latest --secret=$DISCORD_TOKEN_SECRET_NAME)" > .env
 echo 'DATA_STORAGE=datastore' >> /opt/app/.env
-echo "DATASTORE_NAMESPACE=$(curl 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/DATASTORE_NAMESPACE' --silent --fail --show-error --header 'Metadata-Flavor: Google')" >> /opt/app/.env
+echo "DATASTORE_NAMESPACE=$DATASTORE_NAMESPACE" >> /opt/app/.env
 
 # reload app
 cp /opt/app/tools/python-app.conf /etc/supervisor/conf.d/python-app.conf
---
+++ tools/launch_gce_instance.sh
@@ -0,0 +1,5 @@
+gcloud beta compute instances create staging-20210730c --zone=us-east4-c --machine-type=e2-medium \
+    --image-family=debian-10 --image-project=debian-cloud --boot-disk-size=200GB \
+    --scopes=https://www.googleapis.com/auth/cloud-platform \
+    --metadata-from-file=startup-script=gce_startup_script.sh \
+    --metadata=DATASTORE_NAMESPACE=staging,DISCORD_TOKEN_SECRET_NAME=lws-discord-token-staging
